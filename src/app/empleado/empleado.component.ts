a9e2eed9046c4ecfdd1ba159c6fd141974e1cac1
Johann <johann_antonio85@hotmail.com>
2022-11-30 22:32:59 -0600

    first commit

---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  templateUrl: './empleado.component.html',
+ // template: "<p>Aqui iría un empleado gg</p>",
+  styleUrls: ['./empleado.component.css']
+  //styles:["p { background-color: wheat; }"]
+
+})
+export class EmpleadoComponent {
+
+}
