c53c265f0229b9533cbb5c1007d5c92ae2449e9e
Johann-28 <johann.velazquezantn@uanl.edu.mx>
2022-12-06 22:03:55 -0600

    video -10

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -9,5 +9,29 @@ import { Component } from '@angular/core';
 
 })
 export class EmpleadoComponent {
+    nombre = "Johann";
+    appelido = "Velazquez";
+   // private edad = 18;
+    edad = 18;
+    
+    llamaInstitucion(value:String)
+    {
+      
+    }
+    
+    //institucion = "FCFM";
 
+    /*  getEdad()
+    {
+      return this.edad;
+    }*/
+
+    habilitacionCuadro = false;
+    userRegistrado = false;
+
+    getRegistroUsuario()
+    {
+      this.userRegistrado = true;
+      
+    }
 }
